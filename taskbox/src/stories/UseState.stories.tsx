1e3adb20b7b550443edea33c11279b9910b113b9
LizavetaZ <vokibzoj@yandex.by>
2023-08-22 23:10:31 +0300

    use state

---
+++ taskbox/src/stories/UseState.stories.tsx
@@ -0,0 +1,25 @@
+import React, {useState} from "react";
+
+export default {
+    title: 'useState demo'
+}
+
+ function generateData() {
+    console.log('generate data')
+    return 1;
+}
+
+export const Example1 = () => {
+    console.log('Example1')
+
+    const [counter, setCounter] = useState(generateData) //без useMemo React вызовет один раз
+
+    const changer = (state: number) => state+1
+
+    return (<>
+        <button onClick={() => setCounter(changer)}>+</button>
+            {counter}
+        </>
+    )
+
+}
